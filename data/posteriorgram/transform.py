473f79831b81c74b6b7681557252243713522120
roy_li <lilutzu.roy@gmail.com>
2015-12-04 16:52:02 +0800

    Script for adding instance to data

--- data/posteriorgram/transform.py
+++ data/posteriorgram/transform.py
@@ -1,10 +1,9 @@
 #f1 = open("./rnn_input", "r")
 #f2 = open("../MLDS_HW1_RELEASE_v1/label/train.lab", "r")
-f1 = open("./rnn_test", "r")
-f2 = open("./my_test.post", "r")
-myfile = open("./my_test_input", "w")
+f1 = open("./rnn_input", "r")
+f2 = open("./my_train.post", "r")
+myfile = open("./my_train_input2", "w")
 while True:
-  #for i in range(3):
   s1 = f1.readline()
   s2 = f2.readline()
   if s1 == "" or s2 == "": break
